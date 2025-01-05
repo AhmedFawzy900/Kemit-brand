2652e33815af2e4aec04205e18fda94b78948690
AhmedFawzy900 <ahmedattia69513@gmail.com>
2025-01-05 23:41:25 +0200

    fix images

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import Home from "./pages/Home";
 function App() {
   return (
     <>
-      <BrowserRouter>
+
         <Header />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
         </Routes>
-      </BrowserRouter>
+      
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HelmetProvider } from 'react-helmet-async'
+import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
     </HelmetProvider>
   </StrictMode>,
 )
